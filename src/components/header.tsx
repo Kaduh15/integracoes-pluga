4bb4a854be861083225daa74c2764a8cb5ef61ed
Kaduh15 <kadu.silva2014@gmail.com>
2025-12-12 13:53:20 -0300

    feat: atualiza Header com busca de integrações
    - Adiciona campo de busca com ícone de lupa
    - Integra com integrations-store para atualizar searchValue
    - Exibe logo da Pluga, título e descrição
    - Aplica estilização com Tailwind e design responsivo
    
    Header permite busca em tempo real das integrações através do input.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,38 @@
+import { SearchIcon } from 'lucide-react'
+import { useIntegrationsStore } from '@/state/integrations-store'
+
 export function Header() {
-  return <header>This is the header</header>
+  const { actions } = useIntegrationsStore()
+
+  const handlerSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    actions.setSearch(event.target.value)
+  }
+
+  return (
+    <header className="mx-auto flex flex-col items-center justify-center gap-4 p-8">
+      <img src="/logo-pluga.svg" alt="logo da Pluga" />
+
+      <h1 className="font-bold text-4xl text-muted-foreground">
+        Ferramentas Instegradas
+      </h1>
+
+      <p className="w-1/2 text-center font-normal text-accent-foreground/50">
+        Explore as diversas integrações disponíveis para otimizar seus fluxos de
+        trabalho e aumentar sua produtividade.
+      </p>
+
+      <div className="flex w-1/3 items-center gap-2 rounded-md border border-accent-foreground/10 bg-white px-4 py-2 shadow-md">
+        <SearchIcon className="text-accent-foreground/50" />
+        <input
+          autoComplete="off"
+          className="flex-1 focus:outline-none"
+          id="search"
+          name="search"
+          placeholder="Buscar integração..."
+          type="text"
+          onChange={handlerSearch}
+        />
+      </div>
+    </header>
+  )
 }
